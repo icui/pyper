2599b6ffa30002360027602ef2040604976b32b5
Congyue Cui <ccui@princeton.edu>
2020-04-01 10:53:17 -0400

    update

--- example/main.py
+++ example/main.py
@@ -19,30 +19,56 @@ def _test_tasks(target: Block):
     # add a serial task to call a function
     # note that task execution functions should be defined in a seperate file
     # for serial tasks, args (arguments passed to function) should have type tuple
-    target.add(Task(cmd=test, args=(next(_counter),)))
+    # name is an optional argument which is displayed in output/output.log
+    target.add(Task(
+        cmd=test,
+        name='task_with_name',
+        args=(next(_counter),)
+    ))
 
     # add a parallel task to call a function
     # for parallel tasks, args may have type list or tuple
     # if args is tuple, then the same args is used across all ranks
     # if args is list, then it should have a length equal to nranks, each element should be a tuple
     # specify gpus_per_rank (which should be 0 or 1 in most cases) to use GPU
-    target.add(Task(cmd=test, args=(next(_counter),), nranks=3, gpus_per_rank=1))
-    target.add(Task(cmd=test, args=[(next(_counter),), (next(_counter),), (next(_counter),)], nranks=3))
+    target.add(Task(
+        cmd=test,
+        args=(next(_counter),),
+        nranks=3,
+        gpus_per_rank=1
+    ))
+
+    target.add(Task(
+        cmd=test,
+        args=[(next(_counter),), (next(_counter),), (next(_counter),)],
+        nranks=3
+    ))
 
     # add a serial task to execute a shell command
-    target.add(Task(cmd=f'sh _test.sh', args=(next(_counter),)))
+    target.add(Task(
+        cmd='sh _test.sh',
+        args=(next(_counter),)
+    ))
 
     # add a parallel task to execute a shell command
-    target.add(Task(cmd=f'sh _test.sh', args=(next(_counter),), nranks=2))
+    target.add(Task(
+        cmd='sh _test.sh',
+        args=(next(_counter),), nranks=2
+    ))
 
     # add a parallel GPU task to execute a shell command
-    target.add(Task(cmd=f'sh _test.sh', args=(next(_counter),), nranks=2, gpus_per_rank=1))
+    target.add(Task(
+        cmd=f'sh _test.sh',
+        args=(next(_counter),),
+        nranks=2,
+        gpus_per_rank=1
+    ))
 
 
 def _test_blocks(target: Block, add_blocks=False):
     """Demonstrates how to add blocks."""
     # create a block that runs child tasks / blocks in serial
-    child1 = Block()
+    child1 = Block(name='block_with_name')
     target.add(child1)
 
     # create a block that runs child tasks / blocks in parallel
